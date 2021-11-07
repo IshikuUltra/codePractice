9c9ed60e4dfbc31f2f197fd7b462a7fcfeabdd71
Ishiku <samuelgoodnight@icloud.com>
2021-11-07 11:28:39 -0600

    7

--- zedShaw/dll.py
+++ zedShaw/dll.py
@@ -1,5 +1,5 @@
-######################
-class Node:
+#########################################################################################
+class Node: # no need for (object) in P3 
 
     def __init__(self,data):
         self.data = data
@@ -7,45 +7,77 @@ class Node:
         self.last = None
 
     def __repr__(self): 
-        nval = self.data and self.next or None
-        pval = self.last and self.last or None #not complete
-        return f"[D:{self.data}, P:{repr(pval)}, N:{repr(nval)}]"
+        nval = self.data and self.next or None 
+        pval = self.last and self.last.data or None 
+        return f"[D:{self.data}, L:{repr(pval)}, N:{repr(self.next)}]"
 
-class DoubleLinkedList: # no need for (object) in P3 
+
+class DoubleLinkedList: 
 
     def __init__(self):
         self.begin = None
         self.end = None
     
-    def push(self,data):
+    def push(self, data):
         newNode = Node(data)
         if self.begin is None:
             self.begin = newNode
             self.end = self.begin
         else:
             self.end.next = newNode
-            self.end.next.last = self.end.data # Seems legit
-            self.end = newNode
+            self.end.next.last = self.end
+            self.end = newNode #
 
     def pop(self): # remove last node
-        pass
-        # self.end = self.end.last
-        # self.end.next = None
+        self.end = self.end.last
+        self.end.next = None
 
-    def shift(self):
-        pass
+    def count(self):
+        tmp = self.begin
+        count = 0
+        while tmp != None:
+            count += 1
+            tmp = tmp.next
+        return count
 
-    def unshift(self):
-        pass
+    def shift(self,data): #Trying to push 0 on this list disrupts its print
+        newNode = Node(data)
+        newNode.next = self.begin
+        self.begin = newNode
+
+    def unshift(self): #removes first item
+        tmp = self.begin 
+        self.begin = self.begin.next
+
+#######
+#Notes: 
+    #1) node1.last wont point to node0.data
+    #2) when calling detatch node every node.last after the detatched node dont dynamically update
+######
 
     def detach_node(self, node): #removes node from anywhere in list
-        pass
+        tmp = self.begin
+        while tmp:
+            if tmp.data == node:
+                tmp.last.next = tmp.next
+                return tmp
+            tmp = tmp.next
 
-    def remove(self,obj):
-        pass
+    def first(self): 
+        return self.begin
 
-    def first(self):
-        pass
+    def last(self): 
+        return self.end
 
-    def last(self):
-        pass
+
+test = DoubleLinkedList()
+test.push(1)
+test.push(2)
+test.push(3)
+test.shift(100) #shift(0) causes print to fall apart -> solved, its because of your'e nval setup -> 0 is None of some sorts in python
+test.push(4)
+# test.unshift()
+test.detach_node(0)
+# print(test.last())
+print(test.begin) #when shifting 0, self.begin prints-> [D:0, L:None, N:None] -> solved 
+# print(test.begin.next)#shfting 0 does still contain a pointer to node chain
--- zedShaw/sllTests.py
+++ zedShaw/sllTests.py
@@ -4,17 +4,28 @@ from dll import *
 def test_push():
     colors = DoubleLinkedList()
     colors.push(1)
-    # assert colors.count() == 1
+    assert colors.count() == 1
     colors.push(2)
     colors.push(3)
-    # colors.pop()
-    # assert colors.count() == 2
+    colors.pop()
+    assert colors.count() == 2
     return colors.begin
 
-# def test_pop():
-#     colors = DoubleLinkedList()
-#     colors.push("magenta")
-#     colors.push("Alizarin")
-#     return colors.pop()
+def test_pop():
+    colors = DoubleLinkedList()
+    colors.push("magenta")
+    colors.push("Alizarin")
+    return colors.pop()
 
-print(test_push())
+def test_shift():
+    colors = DoubleLinkedList()
+    colors.push(1)
+    colors.push(2)
+    colors.push(3)
+    colors.shift(0) #if you use 0 it fucks it up? WTF
+    colors.push(4)
+    # return colors.begin
+
+# print(test_push())
+# print(test_pop())
+print(test_shift())
